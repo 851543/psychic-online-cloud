de3f3dc5e0e0aaef0ca5c25dc99ac32e470c2ef7
区块链与AI工程师 <130641570+851543@users.noreply.github.com>
2025-09-26 20:31:42 +0800

    教育模块新增课程信息接口完成

--- psychic-online-content/psychic-online-content-api/src/main/java/com/psychic/content/api/CourseBaseInfoController.java
+++ psychic-online-content/psychic-online-content-api/src/main/java/com/psychic/content/api/CourseBaseInfoController.java
@@ -3,6 +3,8 @@ package com.psychic.content.api;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.psychic.base.model.PageParams;
 import com.psychic.base.model.PageResult;
+import com.psychic.content.model.dto.AddCourseDto;
+import com.psychic.content.model.dto.CourseBaseInfoDto;
 import com.psychic.content.model.dto.QueryCourseParamsDto;
 import com.psychic.content.model.po.CourseBase;
 import com.psychic.content.service.CourseBaseInfoService;
@@ -15,12 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description 课程信息编辑接口
  * @author 851543
- * @date 2022/9/6 11:29
  * @version 1.0
+ * @description 课程信息编辑接口
+ * @date 2022/9/6 11:29
  */
-@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
+@Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 @RestController
 public class CourseBaseInfoController {
 
@@ -29,8 +31,13 @@ public class CourseBaseInfoController {
 
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams){
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams) {
         return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
     }
 
+    @ApiOperation("新增课程基础信息")
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto) {
+        return courseBaseInfoService.createCourseBase(1232141425L, addCourseDto);
+    }
 }
