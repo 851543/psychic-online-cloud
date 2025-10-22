b46cc440bd6ace2ff9c0547019c1deac553d4d70
区块链与AI工程师 <130641570+851543@users.noreply.github.com>
2025-10-22 22:39:24 +0800

    新增网关模块转发微服务

--- psychic-online-gateway/src/main/java/com/online/GatewayApplication.java
+++ psychic-online-gateway/src/main/java/com/online/GatewayApplication.java
@@ -1,14 +1,13 @@
-package com.psychic;
+package com.online;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableSwagger2Doc
 @SpringBootApplication
-public class ContentApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContentApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
